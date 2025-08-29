e98f0538e55bcb698dcd8165fc1ccafddfe2e94a
MichaelBoegner <cmikeb@gmail.com>
2025-08-29 12:40:39 +0700

    fixed sizing issues to now fill viewport

--- householder-ui/src/App.tsx
+++ householder-ui/src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.tsx'
 import Footer from './components/Footer/Footer.tsx'
-import Home from './pages/Home'
+import Home from  './pages/Home.tsx'
+import './App.css'
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
       <Navbar />
-      <main>
-        <Home />
-      </main>
+      <Home />
       <Footer />
     </div>
   )
